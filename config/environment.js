5ebd6f16000c8fda84ad95a7dbd5fe7ca1481a4a
Brandon McKenzie <brandon.mckenzie@trms.com>
2017-04-14 22:18:00 -0500

    final tweaks

--- config/environment.js
+++ config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'cablecast-dashboard',
     environment: environment,
-    rootURL: '/',
-    locationType: 'auto',
+    rootURL: '/cablecast/plugins/HUD2/',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
